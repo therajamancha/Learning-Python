c9008ce949a7add1853dabe3cdc16891d0777896
Raj Amancha <78197205+therajamancha@users.noreply.github.com>
2022-04-09 21:12:39 +0530

    first commit

---
+++ Strings.py
@@ -0,0 +1,22 @@
+# stings Method
+name  = "Raj Amancha"
+print("Enter Value : ",name)
+# convert into upper case
+print("Convert into UpperCase : ",name.upper())
+# convert into lower case
+print("Convert into LowerCase : ",name.lower())
+# Search Charcter
+print(name.find("A"))
+# Replace Word
+print(name.replace("Raj","Sundar"))
+# find word in String
+print("Raj" in name)
+
+# Airthmetic in Pythone
+print("Modeule",4 / 2)
+print("Modeule",4 // 2)
+print("Remender",4 % 2)
+print("Multiplication",4 * 2)
+print("Power",4 ** 2)
+print("Addition",4 + 2)
+print("Divition",4 - 2)
---
+++ helloWorld.py
@@ -0,0 +1,29 @@
+# print in python
+print("Hello World")
+print("I am Learning Paython")
+# data type 
+name = "Raj"
+age = 21
+is_adult = True
+#  print concatintion
+print("My Name is",name ,"and I am",age,"Years old")
+# take input from user and Save
+name = input("What is Your Name:")
+
+print("You Enterd Name as",name)
+print("Hello " + name)
+old_age = input("Enter Your old age ")
+# type casting 
+print(int(old_age) + 2) 
+# int()
+# bool()
+# str()
+# float()
+
+# print the sum of two enterd valued
+first = input("Enter First Number : ")
+second = input("Enterd Second Number : ")
+sum = int(first) + int(second)
+
+print("The Sum of",first,"and",second,"is",sum)
+
